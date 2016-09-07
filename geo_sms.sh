1c059dd734ef148814e8e93a0343e429beb205eb
rhdzmota <rohdzmota@gmail.com>
2016-09-07 07:14:58 -0500

    add ITESO

--- geo_sms.sh
+++ geo_sms.sh
@@ -12,33 +12,32 @@ a_lat=20.68984931
 a_lon=-103.444991036		
 
 # Latitude and longitude of reference (origin): university iteso
-b_lat=0
-b_lon=0
 j=0
 
 
 # U I V E R S I T Y
 # Latitude and longitude of reference (origin): home rhm
-# Cafedela:{lat:20.6053712272	, lon:-103.415487669}
-# Estacion:{lat: , lon: }
-# Edif.J  :{lat: , lon: }
-# Edif.T	  :{lat: , lon: }
-# Edif.W  :{lat: , lon: }
-# Edif.D  :{lat: , lon: }
-# Edif.C  :{lat: , lon: }
-# Edif.GYM:{lat: , lon: }
-# ombligo :{lat: , lon: }
+# Cafedela:{lat:20.6053712272, lon:-103.415487669}
+# Estacion:{lat:20.60789339  , lon:-103.41867427 }
+# Sill_t  :{lat:20.60868543  , lon:-103.41601339 }
+# D-I     :{lat:20.60733863  , lon:-103.41713981 }
+# Edif.W  :{lat:20.60817997  , lon:-103.41293266 }
+# Cafe_cet:{lat:20.60714689  , lon:-103.41637681 }
+# Edif.J  :{lat:20.60808981  , lon:-103.41659186 }
+# Edif.GYM:{lat:20.60663732  , lon:-103.41357498 }
+# ombligo :{lat:20.60735495  , lon:-103.41594987 }
+# Arrupe  :{lat:20.60863197  , lon:-103.41431816 }
 
-a_latu=20.68984931	
-a_lonu=-103.444991036		
+
+a_latu=20.607	#+- 0.0021 
+a_lonu=-103.415		#+- 0.0032
 
 # Latitude and longitude of reference (origin): university iteso
-b_latu=0
-b_lonu=0
 ju=0
 
 # variables for leaving 
 leave_home=0
+leave_univ=0
 
 k=1
 i=0
@@ -48,6 +47,8 @@ do
     echo recording... -- iter.num: $k
     
     sleep 50
+    
+    # General use variables
     
     # Get the current location using termux 
     loc=$(termux-location)
@@ -60,6 +61,18 @@ do
     lat=$(echo $loc | jq .latitude)
     lon=$(echo $loc | jq .longitude)
     alt=$(echo $loc | jq .altitude)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     # H O M E
     
@@ -116,7 +129,7 @@ do
         k=0
         echo Done.
         echo	
-        ans=$(echo ${day}: Leaving		 home aprox. at ${time}. Coord - {lat.${lat}, lon.${lon}, alt.${alt}})
+        ans=$(echo ${day}: Leaving	 home aprox. at ${time}. Coord - {lat.${lat}, lon.${lon}, alt.${alt}})
         #echo $ans
         termux-sms-send -n 3314105505 $(echo ${ans})
         termux-sms-send -n 3310097615 $(echo ${ans})
@@ -124,30 +137,95 @@ do
     fi
     
     
-    #if [ $(echo "${lat} > ${ls_lat}" | bc) -eq 1 ] && [ $(echo "${lon} > ${ls_lon}" | bc) -eq 1 ];
-    #then
-    #    if [ $(echo "	${lat} < ${li_lat}" | bc) -eq 1 ] && [ $(echo "${lon} < ${li_lon}" | bc) -eq 1 ];
-    #    then
-    #        if [ "$i" -ne "$j" ];
-    #        then
-    #            j=$(($j-1))
-    #            echo 
-    #            echo Just leaving home. Reseting 'k' variable...
-    #            k=0
-    #            echo Done.
-    #            echo	
-    #            ans=$(echo ${day}: Leaving		 home aprox. at ${time}. Coord - {lat.${lat}, lon.${lon}, alt.${alt}})
-    #            #echo $ans
-    #            termux-sms-send -n 3314105505 $(echo ${ans})
-    #            termux-sms-send -n 3310097615 $(echo ${ans})
-    #        fi
-    #    fi
-    #fi
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     # U N I V E R S I T Y - I T E S O 
     
-    # add_code
+    # limits in lat and long: university  
+    ls_lat=$(echo $a_latu + 0.0021 | bc)
+    li_lat=$(echo $a_latu - 0.0021 | bc)
+    ls_lon=$(echo $a_lonu + 0.0032 | bc)
+    li_lon=$(echo $a_lonu - 0.0032 | bc	)
     
+    # conditional to enter the zone: home
+    if [ $(echo "${lat} < ${ls_lat}" | bc) -eq 1 ] && [ $(echo "${lon} < ${ls_lon}" | bc) -eq 1 ];
+    then
+        if [ $(echo "	${lat} > ${li_lat}" | bc) -eq 1 ] && [ $(echo "${lon} > ${li_lon}" | bc) -eq 1 ];
+        then
+            if [ "$i" -eq "$ju" ];
+            then
+                ju=$(($ju+1))
+                echo 
+                echo Just arriving to ITESO UNIV.. Reseting 'k' variable...
+                k=0
+                echo Done.
+                echo	
+                ans=$(echo ${day}: Arriving ITESO aprox. at ${time}. Coord - {lat.${lat}, lon.${lon}, alt.${alt}})
+                #echo $ans
+                termux-sms-send -n 3314105505 $(echo ${ans})
+                termux-sms-send -n 3310097615 $(echo ${ans})
+            fi
+        fi
+    fi
+    # conditional to leave zone: home
+    if [ $(echo "${lat} > ${ls_lat}" | bc) -eq 1 ] || [ $(echo "${lon} > ${ls_lon}" | bc) -eq 1 ];
+    then
+        if [ "$i" -ne "$ju" ];
+        then
+            # leaving home, turn on leave_home
+            leave_univ=1
+        fi
+    fi
+    
+    if [ $(echo "	${lat} < ${li_lat}" | bc) -eq 1 ] || [ $(echo "${lon} < ${li_lon}" | bc) -eq 1 ];
+    then
+        if [ "$i" -ne "$ju" ];
+        then
+            # leaving home, turn on leave_home
+            leave_univ=1
+        fi
+    fi
+    
+    if [ "$leave_univ" -eq "1" ];
+    then
+        ju=$(($ju-1))
+        echo 
+        echo Just leaving ITESO UNIV. Reseting 'k' variable...
+        k=0
+        echo Done.
+        echo	
+        ans=$(echo ${day}: Leaving		ITESO aprox. at ${time}. Coord - {lat.${lat}, lon.${lon}, alt.${alt}})
+        #echo $ans
+        termux-sms-send -n 3314105505 $(echo ${ans})
+        termux-sms-send -n 3310097615 $(echo ${ans})
+        leave_univ=0
+    fi
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    # E N D
     
     # Autoincrement k-variable
     k=$((k+1))
